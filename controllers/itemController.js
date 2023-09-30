bf883369f5f924c65920642e907aed022b879168
mosmn <ithinkisee8@gmail.com>
2023-09-30 14:49:27 +0800

    added getItem

--- controllers/itemController.js
+++ controllers/itemController.js
@@ -1,2 +1,9 @@
 const Item = require('../models/item');
 const asyncHandler = require("express-async-handler");
+
+// Display details for a specific item
+exports.getItem = asyncHandler(async (req, res) => {
+    const item = await Item.findById(req.params.id).populate('category');
+    res.render('item', {item});
+});
+
--- routes/catalog.js
+++ routes/catalog.js
@@ -2,6 +2,7 @@ const express = require('express');
 const router = express.Router();
 
 const category_controller = require('../controllers/categoryController');
+const item_controller = require('../controllers/itemController');
 
 // Display home page
 router.get('/', category_controller.homePage);
@@ -9,7 +10,7 @@ router.get('/', category_controller.homePage);
 // Display a list of all items in a specific category
 router.get('/category/:id/items', category_controller.getItemsInCategory);
 
-// // Display details for a specific item
-// router.get('/item/:id', getItemDetails);
+// Display details for a specific item
+router.get('/item/:id', item_controller.getItem);
 
 module.exports = router;
