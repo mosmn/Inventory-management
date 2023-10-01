647d6e4c7706e7483437cbae9da29fb68f93b331
mosmn <ithinkisee8@gmail.com>
2023-10-01 11:57:17 +0800

    testing

--- models/item.js
+++ models/item.js
@@ -1,32 +1,26 @@
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
 const Schema = mongoose.Schema;
 
 const ItemSchema = new Schema({
-    name: { type: String, required: true, maxLength: 100 },
-    description: { type: String, required: true, maxLength: 1000 },
-    category: { type: Schema.Types.ObjectId, ref: 'Category' },
-    price: { type: Number, required: true },
-    number_in_stock: { type: Number, required: true },
-    image: { type: String, required: true }
-    });
+  name: { type: String, required: true, maxLength: 100 },
+  description: { type: String, required: true, maxLength: 1000 },
+  category: { type: Schema.Types.ObjectId, ref: "Category" },
+  price: { type: Number, required: true },
+  number_in_stock: { type: Number, required: true },
+  image: { type: String, required: true },
+});
 
-ItemSchema
-    .virtual('url')
-    .get(function () {
-        return '/catalog/item/' + this._id;
-    });
+ItemSchema.virtual("url").get(function () {
+  return "/catalog/item/" + this._id;
+});
 
-ItemSchema
-    .virtual('price_formatted')
-    .get(function () {
-        return '$' + this.price;
-    });
+ItemSchema.virtual("price_formatted").get(function () {
+  return "$" + this.price;
+});
 
-ItemSchema
-    .virtual('number_in_stock_formatted')
-    .get(function () {
-        return this.number_in_stock + ' in stock';
-    });
+ItemSchema.virtual("number_in_stock_formatted").get(function () {
+  return this.number_in_stock + " in stock";
+});
 
-module.exports = mongoose.model('Item', ItemSchema);
+module.exports = mongoose.model("Item", ItemSchema);
