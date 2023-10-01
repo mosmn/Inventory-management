b7956932f902f59a988dce5a5c2b89affc003275
mosmn <ithinkisee8@gmail.com>
2023-10-01 16:06:45 +0800

    deployment setup

--- app.js
+++ app.js
@@ -32,7 +32,7 @@ app.use(limiter);
 // Set up mongoose connection
 const mongoose = require("mongoose");
 mongoose.set("strictQuery", false);
-const mongoDB = process.env.MONGO_URL || process.env.MONGODB_URI;
+const mongoDB = process.env.MONGODB_URI;
 
 main().catch((err) => console.log(err));
 async function main() {
